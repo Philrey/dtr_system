c6e9a16b2e67d5e30d938eee38d870b69e1d6915
Phil Rey <32130982+Philrey@users.noreply.github.com>
2018-06-30 14:27:39 +0800

    added index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	include("functions.php");
+?>
+<!DOCTYPE html>
+<html>
+	<header>
+		<title>DTR System</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</header>
+	
+	<div>
+		<h2>DTR System v1.0</h2>
+		
+		<form method="POST" action="">
+			<input type="text" name="uPin" placeholder="Enter User Pin">
+			<button type="submit" name="submit" value="timeIn">TIME IN</button>
+		</form>
+	</div>
+</html>
